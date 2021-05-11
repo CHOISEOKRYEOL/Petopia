44496915026f121f75281095eec79af6d704f4a8
sunwoo <sun960127@naver.com>
2021-05-11 14:48:50 +0900

    sunwoo

--- app/src/main/java/com/pms/petopia/web/MyTownBoardMainHandler.java
+++ app/src/main/java/com/pms/petopia/web/MyTownBoardMainHandler.java
@@ -2,6 +2,7 @@ package com.pms.petopia.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -48,7 +49,11 @@ public class MyTownBoardMainHandler extends HttpServlet{
         out.println("<input type='submit' value='전송'>");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      StringWriter strWriter = new StringWriter();
+      PrintWriter printWriter = new PrintWriter(strWriter);
+      e.printStackTrace(printWriter);
+
+      out.printf("<pre>%s</pre>\n", strWriter.toString());
     }
     out.println("</body>");
     out.println("</html>");
