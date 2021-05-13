87ba5a11a4399ad9f65d1832411c8000baf983a1
yoontaehoonDev <siryou10@gmail.com>
2021-05-14 01:45:20 +0900

    Q&A List

--- app/src/main/java/com/pms/petopia/web/QnaDetailHandler.java
+++ app/src/main/java/com/pms/petopia/web/QnaDetailHandler.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.pms.petopia.domain.Member;
 import com.pms.petopia.domain.Qna;
 import com.pms.petopia.service.QnaService;
 
@@ -21,11 +20,11 @@ public class QnaDetailHandler extends HttpServlet {
       throws ServletException, IOException {
 
     QnaService qnaService = (QnaService) request.getServletContext().getAttribute("quaService");
-    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
-
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
+
+    int no = Integer.parseInt(request.getParameter("no"));
 
     out.println("<!DOCTYPE html>");
     out.println("<html>");
@@ -35,70 +34,21 @@ public class QnaDetailHandler extends HttpServlet {
     out.println("<body>");
 
     try {
-      Qna q = qnaService.findByNo(loginUser.getNo());
+      Qna q = qnaService.findByNo(no);
       out.println("<h1>Q&A 상세보기</h1>");
-      out.println("<form action='update' method='post'>");
       out.println("<table border = '1'>");
       out.println("<tbody>");
-      if(loginUser.getNo() == q.getWriter().getNo()) {
-        out.println("제목 : <input type='text' name='title'><br>");
-        out.println("내용 : <textarea name='content' rows='10' cols='60'></textarea><br>");
-        out.println("<input type='submit' value='수정'>");
-      }
-      else {
-        out.printf("<tr><th>제목</th><td><input type='text' values='%s' readonly></td></tr>\n", q.getTitle());
-        out.printf("<tr><th>내용</th><td><textarea name='content' rows='10' cols='60' values='%s' readonly></td></tr>\n", q.getContent());
-      }
+      out.println("<form action='update' method='post'>");
+      out.printf("<tr><th>제목</th><td><input type='text' values='%s'></td></tr>\n", q.getTitle());
+      out.printf("<tr><th>내용</th><td><textarea name='content' rows='10' cols='60' values='%s'></td></tr>\n", q.getContent());
+      out.println("<input type='submit' value='수정'></form>");
+      out.println("<a href='delete'>삭제</a>"); 
     }
     catch (Exception e) {
       StringWriter strWriter = new StringWriter();
       PrintWriter printWriter = new PrintWriter(strWriter);
       e.printStackTrace(printWriter);
       out.printf("<pre>%s</pre>\n", strWriter.toString());
-    }
-    out.println("</body>");
-    out.println("</html>");
-  }
-
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    QnaService qnaService = (QnaService) request.getServletContext().getAttribute("qnaService");
-
-
-    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
-
-    if(loginUser == null) {
-      throw new ServletException("로그인 후 이용하세요.");
-    }
-
-    Qna qna = new Qna();
-
-    qna.setTitle(request.getParameter("title"));
-    qna.setContent(request.getParameter("content"));
-    qna.setWriter(loginUser);
-
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>Q&A 등록</title>");
-    out.println("</head>");
-    out.println("<body>");
-
-    try {
-      qnaService.add(qna);
-
-      out.println("<h1>Q&A 등록</h1>");
-
-      response.setHeader("Refresh", "1;url=../main");
-    }
-    catch (Exception e) {
-      StringWriter strWriter = new StringWriter();
-      PrintWriter printWriter = new PrintWriter(strWriter);
-      e.printStackTrace(printWriter);
-
     }
     out.println("</body>");
     out.println("</html>");
--- app/src/main/java/com/pms/petopia/web/QnaListHandler.java
+++ app/src/main/java/com/pms/petopia/web/QnaListHandler.java
@@ -3,6 +3,7 @@ package com.pms.petopia.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,65 +21,53 @@ public class QnaListHandler extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>Q&A 작성</title>");
-    out.println("</head>");
-    out.println("<body>");
-
-    out.println("<h1>Q&A 작성</h1>");
-    out.println("<form action='add' method='post'>");
-    out.println("제목 : <input type='text' name='title'><br>");
-    out.println("내용 : <textarea name='content' rows='10' cols='60'></textarea><br>");
-    out.println("<input type='submit' value='작성하기'>");
-
-  }
-
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     QnaService qnaService = (QnaService) request.getServletContext().getAttribute("qnaService");
-
 
     Member loginUser = (Member) request.getSession().getAttribute("loginUser");
 
-    if(loginUser == null) {
-      throw new ServletException("로그인 후 이용하세요.");
-    }
-
-    Qna qna = new Qna();
-
-    qna.setTitle(request.getParameter("title"));
-    qna.setContent(request.getParameter("content"));
-    qna.setWriter(loginUser);
-
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
 
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
-    out.println("<title>Q&A 등록</title>");
+    out.println("<title>내 Q&A 목록</title>");
     out.println("</head>");
     out.println("<body>");
 
+    out.printf("<h1>%s의 Q&A 목록</h1>\n", loginUser.getNick());
     try {
-      qnaService.add(qna);
+      List<Qna> list = qnaService.list();
 
-      out.println("<h1>Q&A 등록</h1>");
+      out.println("<table border='1'>");
+      out.println("<thead>");
+      out.println("<tr>");
+      out.println("<th>번호</th> <th>제목</th> <th>작성자</th> <th>등록일</th> ");
+      out.println("</tr>");
+      out.println("</thead>");
+      out.println("<tbody>");
 
-      response.setHeader("Refresh", "1;url=../main");
+      for(Qna q : list) {
+        if(q.getWriter().getNo() == loginUser.getNo()) {
+          out.printf("<tr><td>%d</td>"
+              + "<td><a href='detail?no=%1$d'>%s</a></td>"
+              + "<td>%s</td>"
+              + "<td>%s</td></tr>\n", 
+              q.getNo(), q.getTitle(), q.getWriter().getName(), q.getCreatedDate());
+        }
+      }
+      out.println("</tbody>");
+      out.println("</table>");
+
     }
     catch (Exception e) {
       StringWriter strWriter = new StringWriter();
       PrintWriter printWriter = new PrintWriter(strWriter);
       e.printStackTrace(printWriter);
-
     }
+
     out.println("</body>");
     out.println("</html>");
   }
+
 }
