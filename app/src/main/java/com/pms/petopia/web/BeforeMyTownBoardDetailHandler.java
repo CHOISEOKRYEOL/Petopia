58c8c75c60e79e0867e45a3253e6732480918ddb
sunwoo <sun960127@naver.com>
2021-05-17 17:32:08 +0900

    .

--- app/src/main/java/com/pms/petopia/web/BeforeMyTownBoardDetailHandler.java
+++ app/src/main/java/com/pms/petopia/web/BeforeMyTownBoardDetailHandler.java
@@ -6,10 +6,10 @@ import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.pms.petopia.domain.Member;
 import com.pms.petopia.domain.MyTownBoard;
 import com.pms.petopia.domain.MyTownBoardComment;
 import com.pms.petopia.domain.SmallAddress;
@@ -18,8 +18,8 @@ import com.pms.petopia.service.MyTownBoardService;
 import com.pms.petopia.service.SmallAddressService;
 
 @SuppressWarnings("serial")
-@WebServlet("/mytown/detail")
-public class MyTownBoardDetailHandler extends HttpServlet {
+//@WebServlet("/mytown/detail")
+public class BeforeMyTownBoardDetailHandler extends HttpServlet {
 
   SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
   @Override
@@ -30,22 +30,88 @@ public class MyTownBoardDetailHandler extends HttpServlet {
     SmallAddressService smallAddressService = (SmallAddressService) request.getServletContext().getAttribute("smallAddressService");
     MyTownBoardCommentService myTownBoardCommentService = (MyTownBoardCommentService) request.getServletContext().getAttribute("myTownBoardCommentService");
     response.setContentType("text/html;charset=UTF-8");
+
     PrintWriter out = response.getWriter();
     int no = Integer.parseInt(request.getParameter("no"));
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>게시글 상세</title>");
+    out.println("</head>");
+    out.println("<body>");
+
     int stateNo= Integer.parseInt(request.getParameter("stateNo")); //일단 가져왔음
     int cityNo = Integer.parseInt(request.getParameter("cityNo"));
 
     try {
-      MyTownBoard myTownBoard = myTownBoardService.get(no);
-      List<SmallAddress> smallAddresses = smallAddressService.list();
+      MyTownBoard b = myTownBoardService.get(no);
+      if (b == null) {
+        out.println("<p>해당 번호의 게시글이 없습니다.</p>");
+        return;
+      }
+      out.printf("<h1>%s %s</h1>", b.getBigAddress().getName(), b.getSmallAddress().getName());
+      out.println("<h2>게시글 상세보기</h2>");
+      out.printf("<form action='update' method='post'>",
+          b.getBigAddress().getNo(), b.getSmallAddress().getNo());
+      out.println("<table border='1'>");
+      out.println("<tbody>");
+      //
 
-      //      Member loginUser = (Member) request.getSession().getAttribute("loginUser");
-      //      if (loginUser != null && myTownBoard.getWriter().getNo() == loginUser.getNo()) {
+      List<SmallAddress> smallAddress = smallAddressService.list();
+      out.println("<tr><th>광역시/도</th> ");
+      out.println("<td><select name ='stateNo'>\n");
+      for (SmallAddress s : smallAddress) {
+        if (s.getBigAddress().getNo() == stateNo) {
+          out.printf("<option value='%d' selected>%s</option>\n", s.getBigAddress().getNo(), s.getBigAddress().getName());
+        } else {
+          out.printf("<option value='%d'>%s</option>\n", s.getBigAddress().getNo(), s.getBigAddress().getName());
+        }
+      }
+      out.println("</select></td></tr>\n");
 
+      out.println("<tr><th>시/군/구</th> ");
+      out.println("<td><select name='cityNo'>\n");
+      for (SmallAddress s : smallAddress) {
+        if (s.getNo() == cityNo) {
+          out.printf("<option value='%d' selected>%s</option>\n", s.getNo(), s.getName());
+        } else {
+          out.printf("<option value='%d'>%s</option>\n", s.getNo(), s.getName());
+        }
+      }
+      out.println("</select></td></tr><br>\n");
+
+      out.printf("<tr><th>번호</th>"
+          + " <td><input type='text' name='no' value='%d'readonly></td></tr>\n", b.getNo());
+      out.printf("<tr><th>제목</th> "
+          + "<td><input name='title' type='text' value='%s'></td></tr>\n", b.getTitle());
+      out.printf("<tr><th>내용</th> "
+          + "<td><textarea name='content' rows='10'>%s</textarea></td></tr>\n", b.getContent());
+      out.printf("<tr><th>작성자</th> <td>%s</td></tr>\n", b.getWriter().getNick());
+      out.printf("<tr><th>등록일</th> <td>%s</td></tr>\n", formatter.format(b.getCreatedDate()));
+      out.printf("<tr><th>조회수</th> <td>%s</td></tr>\n", b.getViewCount());
+      //out.printf("<tr><th>댓글</th> <td>%s</td></tr>\n",b.getComment());
+      out.println("</tbody>");
+
+      Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+      if (loginUser != null && b.getWriter().getNo() == loginUser.getNo()) {
+
+        out.println("<tfoot>");
+        out.println("<tr><td colspan='2'>");
+        out.println("<input type='submit' value='변경'>"
+            + "<a href='delete?no=" + b.getNo() + "'> 삭제</a> ");
+        out.println("</td></tr>");
+        out.println("</tfoot>");
+      }
+
+      out.println("</table>");
+      out.printf("</form>");
+      out.printf("<a href='list?stateNo=%d&cityNo=%d'>목록</a></p>\n", b.getBigAddress().getNo(), b.getSmallAddress().getNo());
+      out.println("<br>");
       List<MyTownBoardComment> comments = myTownBoardCommentService.list(no);
       int count = 0;
       for(MyTownBoardComment c : comments) {
-        if(myTownBoard.getNo() == c.getMyTownBoard().getNo()) {
+        if(b.getNo() == c.getMyTownBoard().getNo()) {
           out.println("<table border='1'>");
           out.println("<tbody>");
           out.printf("<tr><th>작성자</th><td>%s</td>\n"
@@ -68,7 +134,7 @@ public class MyTownBoardDetailHandler extends HttpServlet {
 
       try {
         out.println("<form action='http://localhost:8080/web/mytowncomment/add' method='post'>");
-        out.printf("<input type='hidden' name='boardNo' value='%d'> <br>\n", myTownBoard.getNo());
+        out.printf("<input type='hidden' name='boardNo' value='%d'> <br>\n", b.getNo());
         out.println("댓글: <textarea name='content' rows='1' cols='30'></textarea><br>");
 
       } catch (Exception e) {
@@ -85,11 +151,6 @@ public class MyTownBoardDetailHandler extends HttpServlet {
       out.println("</form>");
       out.println("</body>");
       out.println("</html>");
-
-
-      request.setAttribute("myTownBoard", myTownBoard);
-      request.setAttribute("smallAddresses", smallAddresses);
-      request.getRequestDispatcher("/jsp/mytownboard/detail.jsp").include(request, response);
 
     } catch (Exception e) {
       throw new ServletException(e);
---
+++ app/src/main/java/com/pms/petopia/web/BeforeMytownBoardListHandler.java
@@ -0,0 +1,151 @@
+package com.pms.petopia.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.pms.petopia.domain.MyTownBoard;
+import com.pms.petopia.domain.SmallAddress;
+import com.pms.petopia.service.MyTownBoardService;
+import com.pms.petopia.service.SmallAddressService;
+
+@SuppressWarnings("serial")
+//@WebServlet("/mytown/list")
+
+public class BeforeMytownBoardListHandler extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    MyTownBoardService myTownBoardService = (MyTownBoardService) request.getServletContext().getAttribute("myTownBoardService");
+    SmallAddressService smallAddressService = (SmallAddressService) request.getServletContext().getAttribute("smallAddressService");
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    int stateNo = Integer.parseInt(request.getParameter("stateNo"));
+    int cityNo = Integer.parseInt(request.getParameter("cityNo"));
+    String keyword = request.getParameter("keyword");
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.printf("<title>우리동네 게시글 목록</title>\n");
+    out.println("</head>");
+    out.println("<body>");
+
+
+
+    try {
+      List<MyTownBoard> boards = myTownBoardService.list(cityNo,stateNo);
+      SmallAddress s = smallAddressService.get(cityNo);
+      out.printf("<h1>%s %s</h1>", s.getBigAddress().getName(), s.getName());
+
+      try {
+        out.println("<form action='list' method='get'>");
+        List<SmallAddress> smallAddress = smallAddressService.list();
+        out.println("광역시/도 : ");
+        out.println("<select name ='stateNo'>\n");
+        for (SmallAddress sa : smallAddress) {
+          if (sa.getBigAddress().getNo() == stateNo) {
+            out.printf("<option value='%d' selected>%s</option>\n", sa.getBigAddress().getNo(), sa.getBigAddress().getName());
+          } else {
+            out.printf("<option value='%d'>%s</option>\n", sa.getBigAddress().getNo(), sa.getBigAddress().getName());
+          }
+        }
+        out.println("</select>\n");
+
+        out.println("시/군/구 : ");
+        out.println("<select name='cityNo'>\n");
+        for (SmallAddress sa : smallAddress) {
+          if (sa.getNo() == cityNo) {
+            out.printf("<option value='%d' selected>%s</option>\n", sa.getNo(), sa.getName());
+          } else {
+            out.printf("<option value='%d'>%s</option>\n", sa.getNo(), sa.getName());
+          }
+        }
+        out.println("</select>\n");
+
+        out.println("<input type='submit' value='찾기'>");
+
+      } catch (Exception e) {
+        StringWriter strWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(strWriter);
+        e.printStackTrace(printWriter);
+
+        out.printf("<pre>%s</pre>\n", strWriter.toString());
+      }
+      out.println("</form>");
+
+      out.printf("<p><a href='add?stateNo=%d&cityNo=%d'>새 글</a><p>", s.getBigAddress().getNo(), s.getNo());
+      if(boards.size() > 0) {
+
+
+        if (keyword != null && keyword.length() > 0) {
+          boards = myTownBoardService.search(stateNo, cityNo, keyword);
+        } else {
+          boards = myTownBoardService.list(stateNo, cityNo);
+        }
+
+        out.println("<table border='1'>");
+        out.println("<thead>");
+        out.println("<tr>");
+        out.println("<th>번호</th> <th>제목</th> <th>작성자</th> <th>등록일</th> <th>조회수</th> <th>댓글수</th>");
+        out.println("</tr>");
+        out.println("</thead>");
+        out.println("<tbody>");
+
+
+        for (MyTownBoard b : boards) {
+          out.printf("<tr>"
+              + " <td>%d</td>"
+              + " <td><a href='detail?stateNo=%d&cityNo=%d&no=%1$d'>%s</a></td>"
+              + " <td>%s</td>"
+              + " <td>%s</td>"
+              + " <td>%d</td>"
+              + " <td>%d</td></tr>\n",
+              b.getNo(),
+              s.getBigAddress().getNo(),
+              s.getNo(),
+              b.getTitle(), 
+              b.getWriter().getNick(),
+              b.getCreatedDate(),
+              b.getViewCount(),
+              b.getCommentCount());
+        }
+
+        out.println("</tbody>");
+        out.println("</table>");
+
+        out.println("<form action='list' method='get'>");
+        out.printf("<input type='search' name='keyword' value='%s'>\n",
+            keyword != null ? keyword : "");
+        out.printf("<input type='hidden' name='stateNo' value='%d'><br>\n",
+            s.getBigAddress().getNo());
+        out.printf("<input type='hidden' name='cityNo' value='%d'><br>\n",
+            s.getNo());
+        out.println("<button> 검색 </button>");
+        out.println("</form>");
+      } else {
+        out.println("우리동네에 게시글이 없습니다.");
+      }
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+
+
+}
+
+
+
+
+
+
