0eabcd054ee6adca50f8374cb28daa912636f78d
sunwoo <sun960127@naver.com>
2021-05-22 11:41:26 +0900

    .

---
+++ app/src/main/java/com/pms/petopia/domain/Scrap.java
@@ -0,0 +1,28 @@
+package com.pms.petopia.domain;
+
+public class Scrap {
+
+  private int no;
+  private Member member;
+  private Story story;
+
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public Member getMember() {
+    return member;
+  }
+  public void setMember(Member member) {
+    this.member = member;
+  }
+  public Story getStory() {
+    return story;
+  }
+  public void setStory(Story story) {
+    this.story = story;
+  }
+
+}
