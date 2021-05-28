edac454bb222941829a582074b74aa2b23334e77
sunwoo <sun960127@naver.com>
2021-05-28 18:59:46 +0900

    ..

--- app/src/main/java/com/pms/petopia/web/ScrapCheckHandler.java
+++ app/src/main/java/com/pms/petopia/web/ScrapCheckHandler.java
@@ -10,9 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.pms.petopia.domain.Member;
 import com.pms.petopia.domain.Scrap;
-import com.pms.petopia.domain.Story;
 import com.pms.petopia.service.ScrapService;
-import com.pms.petopia.service.StoryService;
 
 @SuppressWarnings("serial")
 @WebServlet("/story/scrapcheck")
@@ -22,46 +20,29 @@ public class ScrapCheckHandler extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    StoryService storyService = (StoryService) request.getServletContext().getAttribute("storyService");
     ScrapService scrapService = (ScrapService) request.getServletContext().getAttribute("scrapService");
 
     PrintWriter out = response.getWriter();
 
     try {
-      String keyword = request.getParameter("keyword");
-      List<Story> storys = null;
-      if (keyword != null && keyword.length() > 0) {
-        storys = storyService.search(keyword);
-      } else {
-        storys = storyService.list();
-      }
+
+      int newsNo = Integer.parseInt(request.getParameter("no"));
+
       List<Scrap> scrapList = null;
       if ((Member)request.getSession().getAttribute("loginUser") != null) {
         Member loginUser = (Member)request.getSession().getAttribute("loginUser");
 
         scrapList = scrapService.list(loginUser.getNo());
-        System.out.println("여");
-        if (scrapList.size() == 0) {
-          for (Story story : storys) {
-            System.out.println("beforescrap" + story.getNo());
-            out.print("beforescrap");
-          }
-        } else {
 
-          for (Scrap scrap : scrapList) {
-            for(Story story : storys) {
-              if (scrap.getStory().getNo() != story.getNo() || scrap == null) {
-                System.out.println("beforescrap" + story.getNo());
-                out.print("beforescrap");
-              } else {
-                System.out.println("alreadyscrap" + story.getNo());
-                out.print("alreadyscrap");
-              }
-            }
+        for (Scrap scrap : scrapList) {
+          if (scrap.getMember().getNo() == loginUser.getNo() && scrap.getStory().getNo() == newsNo) {
+            out.print("1");
+          } else {
+            out.print("0");
           }
-
         }
       }
+
 
     } catch (Exception e) {
       throw new ServletException(e);
