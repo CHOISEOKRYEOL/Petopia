dc0a14ab6804adc2b95c59f24631e7f894faa4ec
sunwoo <sun960127@naver.com>
2021-05-31 21:02:54 +0900

    list,add,main

--- app/src/main/java/com/pms/petopia/web/MainHandler.java
+++ app/src/main/java/com/pms/petopia/web/MainHandler.java
@@ -2,16 +2,14 @@ package com.pms.petopia.web;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainHandler {
 
-  @RequestMapping("main")
-  public void execute(HttpServletRequest request, HttpServletResponse response)
+  @RequestMapping("/main")
+  public void execute()
       throws ServletException, IOException {
 
   }
