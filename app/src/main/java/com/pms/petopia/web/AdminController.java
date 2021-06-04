62aba5221a582661ffd255f2758bf18d7987a433
yoontaehoonDev <siryou10@gmail.com>
2021-06-04 12:21:51 +0900

    Path Fix

--- app/src/main/java/com/pms/petopia/web/AdminController.java
+++ app/src/main/java/com/pms/petopia/web/AdminController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.pms.petopia.domain.Hospital;
 import com.pms.petopia.domain.Member;
@@ -110,7 +111,7 @@ public class AdminController {
     }
   }
 
-  @GetMapping("answer")
+  @PostMapping("answer")
   public String answerQna(Qna qna) throws Exception {
 
     qna.setState(1);
